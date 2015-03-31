9677949e5b16a00a402ba497b0c7bec246d8f1dc
Thomas Deegan <tadeegan@gmail.com>
2015-03-31 16:24:40 -0400

    better debugging and fix cops bug

--- src/java/org/apache/cassandra/service/StorageProxy.java
+++ src/java/org/apache/cassandra/service/StorageProxy.java
@@ -644,8 +644,8 @@ public class StorageProxy implements StorageProxyMBean
                 assert !command.isDigestQuery();
                 logger.debug("Command/ConsistencyLevel is {}/{}", command, consistency_level);
 
-                List<InetAddress> endpoints = StorageService.instance.getLiveNaturalEndpoints(command.table,
-                                                                                              command.key);
+                List<InetAddress> endpoints = StorageService.instance.getLiveNaturalEndpoints(command.table, command.key);
+		logger.debug("end points: " + endpoints);
                 DatabaseDescriptor.getEndpointSnitch().sortByProximity(FBUtilities.getBroadcastAddress(), endpoints);
 
                 RowDigestResolver resolver = new RowDigestResolver(command.table, command.key);
@@ -671,7 +671,7 @@ public class StorageProxy implements StorageProxyMBean
                 if (handler.endpoints.size() == 1)
                     continue;
 
-                assert false : "COPS2 doesn't handle multiple replicas in a DC yet";
+                //assert false : "COPS2 doesn't handle multiple replicas in a DC yet";
 
                 // send the other endpoints a digest request
                 ReadCommand digestCommand = command.copy();
