2ace2860b21d12e8b4f6830a5f26bcb56bb424c6
Thomas Deegan <tadeegan@gmail.com>
2015-05-07 14:57:53 -0500

    added some sleep

--- deegan_client/src/TestClient.java
+++ deegan_client/src/TestClient.java
@@ -371,6 +371,7 @@ public class TestClient {
         
         int topkey = 0;
     	for(int i = 0; i < numOps; i++){
+    		Thread.sleep(20);
     		print(((double)i/(double)numOps*100.0) + "%");
     		if(Math.random() > chanceOfWrite) {
     			// Do a get on one of the keys we wrote
